5244349a10a24980e0c72a7c86db4ff15d6997b2
selvaganeshc1 <selvaganesh.c@justintegrateit.com>
2015-05-26 14:13:56 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ShopifyTest::Application.config.secret_key_base = '5ed51c4ed90927324c6a50baf8d1c7edf463cf7fd12efa68f0e822776b6d5a32b35cc77af12a42943ef99a527f85c6ebb110ae8234ef333c81d72ad5925ada98'
